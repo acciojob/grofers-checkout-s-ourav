3cc92580908f4089c7e871f15f32b41a638e7d0e
Sourav Mazumdar <sourav.mozu@gmail.com>
2024-08-01 19:42:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
-};
+	table=document.querySelector('table')
+	prices=Array.from((table.querySelectorAll('.price')))
+	prices=prices.map(value => Number(value.textContent))
+	// console.log(prices);
+	sum=prices.reduce((sum,value) => sum+value,0)
+	newRow= document.createElement('tr')
+	sumVal=document.createElement('td')
+	sumVal.textContent=sum
+	newRow.appendChild(sumVal)
+	table.appendChild(newRow)
+}
 
 getSumBtn.addEventListener("click", getSum);
 
